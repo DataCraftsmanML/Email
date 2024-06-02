025367ea5b65d3a73cf16a68881e1dc76511be34
Jiya Savaliya <126229948+Jiyasavaliya@users.noreply.github.com>
2024-06-02 16:49:33 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,14 @@
+Email = input("Enter Email Id : ")
+print(Email)
+
+Password = input("Enter Password : ")
+print(Password)
+
+Correct_Email = "jiasavaliya55"
+Correct_Password = "123456"
+
+if "jiasavaliya55" in Email and "123456" in Password:
+ print("Login Successfully")
+
+else:
+    print("Login Failed, Please Try Again!")
